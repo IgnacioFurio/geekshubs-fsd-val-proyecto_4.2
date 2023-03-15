285f012ff6d35a905a178c3bdd4b1cb5ec02f771
Ignacio Furió José <bichoifj@gmail.com>
2023-03-15 19:25:13 +0100

    chore: add input for email and password at Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { InputType } from '../../common/InputType/InputType';
+import './Login.css'
 
 export const Login = () => {
     return (
-        <>
+        <div className='loginDesign'>
             <InputType 
                 className={'inputBasicDesign'}
+                type={'email'}
+                name={'email'}
+                placeholder={'example@email.com'}
             />
-        </>
+            <InputType 
+                className={'inputBasicDesign'}
+                type={'password'}
+                name={'password'}
+                placeholder={'password (egg, 12345)'}
+            />
+        </div>
     );
 };
