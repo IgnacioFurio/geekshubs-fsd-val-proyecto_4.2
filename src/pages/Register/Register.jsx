4f1b7d1042918c46906777564fb13611aadc68c9
Ignacio Furió José <bichoifj@gmail.com>
2023-03-17 13:52:20 +0100

    chore: prepare hook for Register new user

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { InputType } from '../../common/InputType/InputType';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Register.css';
 
 export const Register = () => {
+
+  //HOOKS
+
+  //set data for the new register
+  const [newRegister, setNewRegister] = useState(
+    {
+      user: '',
+      email: '',
+      password: ''
+    }
+  );
+
   return (
-    <div>REsgistrate en Register</div>
+    <Container fluid className='registerDesign'>
+      <Row>
+        <Col xs={1}></Col>
+        <Col xs={10}>
+        <InputType 
+                        className={'inputBasicDesign'}
+                        type={'text'}
+                        name={'user'}
+                        placeholder={'Eddieden06'}
+                        required={true}
+                        // changeFunction={(e)=>inputHandler(e)}
+                        // blurFunction={(e)=>checkError(e)}
+                    />
+        </Col>
+        <Col xs={1}></Col>
+      </Row>
+      <Row>
+        <Col xs={1}></Col>
+        <Col xs={10}>
+        <InputType 
+                        className={'inputBasicDesign'}
+                        type={'email'}
+                        name={'email'}
+                        placeholder={'example@email.com'}
+                        required={true}
+                        // changeFunction={(e)=>inputHandler(e)}
+                        // blurFunction={(e)=>checkError(e)}
+                    />
+        </Col>
+        <Col xs={1}></Col>
+      </Row>
+      <Row>
+        <Col xs={1}></Col>
+        <Col xs={10}>
+        <InputType 
+                        className={'inputBasicDesign'}
+                        type={'password'}
+                        name={'password'}
+                        placeholder={'password (egg, 12345)'}
+                        required={true}
+                        // changeFunction={(e)=>inputHandler(e)}
+                        // blurFunction={(e)=>checkError(e)}
+                    />
+        </Col>
+        <Col xs={1}></Col>
+      </Row>
+    </Container>
   );
 };
