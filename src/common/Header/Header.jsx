8c90b83c8f174f6fbebdec8890ae8530fb8bef82
Ignacio Furió José <bichoifj@gmail.com>
2023-03-20 13:34:25 +0100

    feat: persist Redux inside local storage

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+//  redux 
+import { useDispatch, useSelector } from 'react-redux';
+import { userData } from '../../pages/Slices/userSlice';
+// render 
 import { Navigator } from '../Navigator/Navigator';
 import './Header.css'
 
 export const Header = () => {
+
+    const dataRdx = useSelector(userData);
+
+    const  dispatch = useDispatch();
+
     return (
     <div className='headerDesign'>
         <Navigator route={'Home'} destination={'/'}/>
--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //redux
 import { Provider } from 'react-redux';
 import store from './app/store';
+// redux-persist 
+import { PersistGate } from 'redux-persist/integration/react'
+import persistStore from 'redux-persist/es/persistStore'
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>  
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>  
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
